6efc4818e1cc5443e10a067e8cf3983a852c3183
Mahesh <maheshd95@gmail.com>
2019-07-13 21:43:06 +0530

    Map added to show location

--- src/app/view-student/view-student.component.ts
+++ src/app/view-student/view-student.component.ts
@@ -1,7 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Location } from '../classes/location';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { AngularFirestore } from '@angular/fire/firestore';
+import OlMap from 'ol/Map';
+import OlXYZ from 'ol/source/XYZ';
+import OlTileLayer from 'ol/layer/Tile';
+import OlView from 'ol/View';
+import OlVectorLayer from 'ol/layer/Vector';
+import OlVectorSource from 'ol/source/Vector';
+import OlFeature from 'ol/Feature';
+import OlPoint from 'ol/geom/Point';
+import Style from 'ol/style/Style'
+import Icon from 'ol/style/Icon';
+import ol from 'openlayers';
+import { fromLonLat } from 'ol/proj'
 
 @Component({
   selector: 'app-view-student',
@@ -11,7 +23,15 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class ViewStudentComponent implements OnInit {
 
   public locations: Location[];
-  
+
+  map: OlMap;
+  source: OlXYZ;
+  layer: OlTileLayer;
+  view: OlView;
+  vectorLayer: OlVectorLayer;
+  marker: OlFeature;
+  vectorSource: OlVectorSource;
+
   constructor(private db : AngularFireDatabase, private firestore : AngularFirestore) {   
     
   }
@@ -24,25 +44,96 @@ export class ViewStudentComponent implements OnInit {
         } as Location;
       })
     });
-    console.log(this.locations);
+
+    this.source = new OlXYZ({
+      url: 'http://tile.osm.org/{z}/{x}/{y}.png'
+    });
+
+
+    this.layer = new OlTileLayer({
+      source: this.source
+    });
+
+    this.view = new OlView({
+      center: fromLonLat([80.6337, 7.8]),
+      zoom: 7
+    });
+
+    this.map = new OlMap({
+      target: 'map',
+      layers: [this.layer],
+      view: this.view  
+    });
+   
   }
+
+  //7.2906° N, 80.6337° E
 
   getLocationList(){
     return this.firestore.collection('location').snapshotChanges();
   }
 
-  onPanelOpen(index : string){
-    let location: Location = this.locations[0];
-    let count = 0;
-    while(location.username !== index){
-      location = this.locations[count];
-      count++;
-    }
+  onPanelOpen(curlocations : Location){   
+    this.marker = new OlFeature({
+      // Added fromLonLat
+      geometry: new OlPoint(fromLonLat([curlocations.lat, curlocations.lng]))
+    });
+
+    var featureStyle = new Style({
+      image: new Icon({
+        src: 'https://upload.wikimedia.org/wikipedia/commons/5/50/Red_dot_2.png',
+        scale: 0.3
+      })
+    })
+
+    this.marker.setStyle(featureStyle);
+
+    this.vectorSource = new OlVectorSource({
+        features: [this.marker]
+    });
+
+    this.vectorLayer = new OlVectorLayer({
+        source: this.vectorSource
+    });
+
+    this.source = new OlXYZ({
+      url: 'http://tile.osm.org/{z}/{x}/{y}.png'
+    });
+
+
+    this.layer = new OlTileLayer({
+      source: this.source
+    });
+
+    this.view = new OlView({
+      center: fromLonLat([curlocations.lat, curlocations.lng]),
+      zoom: 11
+    });
+    
+    this.map.getLayers().forEach(layer => this.map.removeLayer(layer));
+    this.map.addLayer(this.layer);
+    this.map.addLayer(this.vectorLayer);
+    this.map.setView(this.view); 
+    this.map.getLayers().forEach(layer => layer.getSource().refresh());
   }
 
+  onPanelClose(){
+    this.source = new OlXYZ({
+      url: 'http://tile.osm.org/{z}/{x}/{y}.png'
+    });
 
-  onSubmit(){
-    
+    this.layer = new OlTileLayer({
+      source: this.source
+    });
+
+    this.view = new OlView({
+      center: fromLonLat([80.6337, 7.8]),
+      zoom: 7
+    });
+
+    this.map.getLayers().forEach(layer => this.map.removeLayer(layer));
+    this.map.addLayer(this.layer);
+    this.map.setView(this.view);     
   }
 
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAL345SrRIT2-116XZe7o9_MkcDhPXPE30",
+    authDomain: "dengue-control-app.firebaseapp.com",
+    databaseURL: "https://dengue-control-app.firebaseio.com",
+    projectId: "dengue-control-app",
+    storageBucket: "",
+    messagingSenderId: "999597754718",
+    appId: "1:999597754718:web:f2066ae0f714bcd7"
+  }
 };
